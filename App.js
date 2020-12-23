b5f399fb4138e4eb1c04ddd7d0e24001c01a30b0
Kolyunya <oleynikovny@mail.ru>
2020-12-23 19:53:27 +0300

    Initialize repository

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView 
+      style = {{
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <Marker
+        key = 'marker'
+        coordinate = {{
+          latitude: 0,
+          longitude: 0,
+        }}
+        icon = { require('./assets/marker.png') }
+      />
+    </MapView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
